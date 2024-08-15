3f4147c73457c0afdb7940270ff02d76ca2ac011
Marianna <3679648+mariannactx@users.noreply.github.com>
2024-08-15 18:37:08 -0300

    Commit inicial

---
+++ src/test/java/br/com/mariannactx/eleicao/EleicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mariannactx.eleicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
